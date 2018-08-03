d14d1a1a39140aa50422085e979079e821ce051b
AntonZUZ <AntonZUZZ@yandex.ru>
2018-08-03 10:45:35 +0300

    Initial commit

---
+++ src/main/java/GroupOfPeople.java
@@ -0,0 +1,15 @@
+import java.lang.management.OperatingSystemMXBean;
+
+public abstract class GroupOfPeople {
+
+    private int population;
+    private int moral;
+
+    public GroupOfPeople(int populationC, int moralC) {
+        population = populationC;
+        moral = moralC;
+    }
+
+    
+
+}
